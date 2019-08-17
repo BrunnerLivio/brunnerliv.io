32b7c0a39207425b88ab7dd22560d79631bfd1e8
Livio <livio.brunner.lb1@gmail.com>
2019-08-17 15:49:24 +0200

    Extract metadata into dedicated files

---
+++ content/site-metadata.js
@@ -0,0 +1,12 @@
+const projects = require('./projects.json');
+const contributions = require('./contributions.json');
+const navigation = require('./navigation.json');
+
+module.exports = {
+  title: `brunnerliv.io`,
+  description: `The personal website of Livio Brunner.`,
+  author: `@BunnerLivio`,
+  navigation: navigation,
+  projects: projects,
+  contributions: contributions,
+};
