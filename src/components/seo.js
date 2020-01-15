25c69387ea838ad87f760d1b9b51ff50c38816b1
Livio Brunner <livio.brunner.lb1@gmail.com>
2020-01-15 20:51:17 +0100

    Add mobile web app capable meta tag

--- src/components/seo.js
+++ src/components/seo.js
@@ -67,6 +67,10 @@ function SEO({ description, lang, meta, title }) {
           name: `twitter:description`,
           content: metaDescription,
         },
+        {
+          name: "apple-mobile-web-app-capable",
+          content: "yes",
+        },
       ].concat(meta)}
     />
   )
