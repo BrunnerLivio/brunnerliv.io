01ef7f0a1911ab8d2b990ae80c576ce2a2a9ec34
Livio <livio.brunner.lb1@gmail.com>
2021-03-14 02:13:13 +0100

    improve spacing in talks on mobile

--- src/pages/talks.js
+++ src/pages/talks.js
@@ -5,13 +5,18 @@ import styled from "styled-components"
 import SEO from "../components/seo"
 import Content from "../components/content"
 import Talk from "../components/talk"
+import breakpoint from "styled-components-breakpoint"
 
 const TalkList = styled.main`
   display: flex;
   align-items: center;
   flex-direction: column;
   width: 100%;
+  margin: 48px 0;
+
+  ${breakpoint("sm")`
   margin: 32px 0;
+    `}
 `
 
 const Talks = ({ data }) => {
