ec6452007c890ca551a97af69a295a4678e00c3f
Livio Brunner <livio.brunner.lb1@gmail.com>
2022-05-11 10:54:23 +0200

    refactor: minify theming

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,8 +4,9 @@ const fs = require("fs")
 exports.onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
     <script
+      // Theming setup
       dangerouslySetInnerHTML={{
-        __html: fs.readFileSync("./init-theme.js", "utf-8"),
+        __html: `!function(){function e(e){window.__theme=e,document.documentElement.className="dark"===e?"dark":""}let t;window.__setPreferredTheme=function(t){e(t);try{localStorage.setItem("preferred-theme",t)}catch(e){}};try{t=localStorage.getItem("preferred-theme")}catch(e){}let r=window.matchMedia("(prefers-color-scheme: dark)");e(t||(r.matches?"dark":"light"))}();`
       }}
     />,
   ])
